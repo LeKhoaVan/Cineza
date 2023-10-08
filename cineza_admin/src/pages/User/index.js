c1c247831927a2c522b054925827a5d450fe2d6a
toduchieu <toduchieu1245@gmail.com>
2023-10-08 16:50:40 +0700

    add btn

--- cineza_admin/src/pages/User/index.js
+++ cineza_admin/src/pages/User/index.js
@@ -1,89 +1,111 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import axios from "axios";
-import DataTable from 'react-data-table-component';
+import DataTable from "react-data-table-component";
 import Table from "../../components/Table";
 import UserDetail from "../UserDetail";
 import formatDate from "../../components/util";
-import "./user.css"
-
+import iconAdd from "../../assets/imageButtons/iconAdd.png";
+import "./user.css";
 
 const columns = [
-    // dùng để sử dụng DataTable
-    // {
-    //     title: 'Code',
-    //     selector: row => row.code,
-    //     // sortable: true
-    // },
-    {
-        title: 'Code',
-        data: "code",
-        // sortable: true
-    },
-    {
-        title: 'Tên',
-        data: "fullName"
-    },
-    {
-        title: "Số điện thoại",
-        data: "numberPhone"
-    },
-    {
-        title: 'Level',
-        data: "level",
-    },
-    {
-        title: 'Ngày sinh',
-        data: "dateOfBirth",
-    },
-    {
-        title: 'Status',
-        data: "status",
-    },
+  // dùng để sử dụng DataTable
+  // {
+  //     title: 'Code',
+  //     selector: row => row.code,
+  //     // sortable: true
+  // },
+  {
+    title: "Code",
+    data: "code",
+    // sortable: true
+  },
+  {
+    title: "Tên",
+    data: "fullName",
+  },
+  {
+    title: "Số điện thoại",
+    data: "numberPhone",
+  },
+  {
+    title: "Level",
+    data: "level",
+  },
+  {
+    title: "Ngày sinh",
+    data: "dateOfBirth",
+  },
+  {
+    title: "Status",
+    data: "status",
+  },
 ];
 
 const User = () => {
-    const [context, setContext] = useState([]);
+  const [context, setContext] = useState([]);
 
-    const [openModalDetail, setOpenModalDetail] = useState(false);
-    const [codeUser, setCodeUser] = useState("");
+  const [openModalDetail, setOpenModalDetail] = useState(false);
+  const [openModelAdd, setOpenModelAdd] = useState(false);
+  const [codeUser, setCodeUser] = useState("");
 
-    const onHandleSelect = (row) => {
-        console.log(row)
-        setCodeUser(row)
-        setOpenModalDetail(!openModalDetail);
-    }
+  const onHandleSelect = (row) => {
+    console.log(row);
+    setCodeUser(row);
+    setOpenModalDetail(!openModalDetail);
+  };
 
-    const onClickHandleCloseP = async () => {
-        window.location.href = "/cineza/admin/users";
-        setOpenModalDetail(false);
-    }
+  const onClickHandleCloseP = async () => {
+    window.location.href = "/cineza/admin/users";
+    setOpenModalDetail(false);
+  };
 
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const result = await axios.get("http://localhost:9000/cineza/api/v1/value/user/get-all");
-                if (result.status == 200) {
-                    const dataSetup = result.data.map((item) => {
-                        return {
-                            ...item,
-                            dateOfBirth: formatDate(item.dateOfBirth)
-                        }
-                    });
-                    setContext(dataSetup);
-                }
-            } catch (error) {
-                console.log("error get api all user " + error)
-            }
-        };
+  const onClickHandleBtnAdd = () => {
+    setOpenModelAdd(true);
+  };
 
-        getData();
-    }, [])
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const result = await axios.get(
+          "http://localhost:9000/cineza/api/v1/value/user/get-all"
+        );
+        if (result.status == 200) {
+          const dataSetup = result.data.map((item) => {
+            return {
+              ...item,
+              dateOfBirth: formatDate(item.dateOfBirth),
+            };
+          });
+          setContext(dataSetup);
+        }
+      } catch (error) {
+        console.log("error get api all user " + error);
+      }
+    };
 
-    return (
-        <div className="user-container">
-            <h3>Người dùng</h3>
-            <div className="table-all-user">
-                {/* <DataTable
+    getData();
+  }, []);
+
+  return (
+    <div className="user-container">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          paddingRight: "10px",
+          alignItems: "center",
+        }}
+      >
+        <h3>Người dùng</h3>
+        <img
+          src={iconAdd}
+          alt="btn-add"
+          className="user-btn-add"
+          onClick={onClickHandleBtnAdd}
+        />
+      </div>
+      <div className="table-all-user">
+        {/* <DataTable
                     columns={columns}
                     data={context != null ? (context.length != 0 ? context : "") : ""}
                     fixedHeader
@@ -92,11 +114,23 @@ const User = () => {
                     onRowClicked={(row) => onHandleSelect(row)}
                     actions
                 /> */}
-                <Table column={columns} data={context} onRowClick={onHandleSelect} />
-                {openModalDetail && <UserDetail codeUserBy={codeUser} onClickHandleClose={onClickHandleCloseP} />}
-            </div>
-        </div>
-    )
+        <Table column={columns} data={context} onRowClick={onHandleSelect} />
+        {openModalDetail && (
+          <UserDetail
+            codeUserBy={codeUser}
+            onClickHandleClose={onClickHandleCloseP}
+          />
+        )}
+        {openModelAdd && (
+          <UserDetail
+            addBtn={true}
+            codeUserBy={codeUser}
+            onClickHandleClose={onClickHandleCloseP}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default User;
--- cineza_server/src/config/configDB.js
+++ cineza_server/src/config/configDB.js
@@ -1,27 +1,29 @@
 const development = {
-    "username": "root",
-    "password": "1234",
-    "database": "cineza",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  username: "root",
+  password: "hieu2010",
+  database: "cineza",
+  host: "127.0.0.1",
+  dialect: "mysql",
 };
 
 const test = {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  username: "root",
+  password: null,
+  database: "database_test",
+  host: "127.0.0.1",
+  dialect: "mysql",
 };
 
 const production = {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-}
+  username: "root",
+  password: null,
+  database: "database_production",
+  host: "127.0.0.1",
+  dialect: "mysql",
+};
 
 module.exports = {
-    development, test, production
-}
+  development,
+  test,
+  production,
+};
