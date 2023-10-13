a83089714d94f09feaf49f499472fbfae85617c1
toduchieu <toduchieu1245@gmail.com>
2023-10-13 09:14:21 +0700

    a

--- cineza_server/src/routes/promotionHeaderRouter.js
+++ cineza_server/src/routes/promotionHeaderRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const { getAllPromotionHeaderController,
-    createPromotionHeaderController } = require("../controller/promotionHeaderController");
+    createPromotionHeaderController,
+    getPromotionHeaderByCodeController } = require("../controller/promotionHeaderController");
 const promotionHeaderRouter = express.Router();
 
 promotionHeaderRouter.get("/get-all", getAllPromotionHeaderController);
 promotionHeaderRouter.post("/create", createPromotionHeaderController);
-// promotionHeaderRouter.get("/get-code/:code",);
+promotionHeaderRouter.get("/get-code/:code", getPromotionHeaderByCodeController);
 
 module.exports = promotionHeaderRouter;
