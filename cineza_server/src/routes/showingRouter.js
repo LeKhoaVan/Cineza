388a88500b5c5ce09d8688aac688b7e55fecf42f
toduchieu <toduchieu1245@gmail.com>
2023-10-25 00:22:19 +0700

    update show

--- cineza_server/src/routes/showingRouter.js
+++ cineza_server/src/routes/showingRouter.js
@@ -1,13 +1,17 @@
 const express = require("express");
 
-const { getAllShowController,
-    createShowController,
-    getAllShowByMovieController,
-    getAllShowByRapController } = require("../controller/showingController")
+const {
+  getAllShowController,
+  getShowByCodeController,
+  createShowController,
+  getAllShowByMovieController,
+  getAllShowByRapController,
+} = require("../controller/showingController");
 
-const showingRouter = express.Router()
+const showingRouter = express.Router();
 
 showingRouter.get("/get-all", getAllShowController);
+showingRouter.get("/get-by-code/:code", getShowByCodeController);
 showingRouter.post("/create", createShowController);
 showingRouter.get("/get-all-by-movie/:codeMovie", getAllShowByMovieController);
 showingRouter.get("/get-all-by-rap/:codeRap", getAllShowByRapController);
--- cineza_server/src/services/showingService.js
+++ cineza_server/src/services/showingService.js
@@ -1,54 +1,67 @@
-const { db } = require("../models/index")
+const { db } = require("../models/index");
 
 const getAllShowService = async () => {
-    const allShow = await db.Showing.findAll();
-    return allShow;
-}
+  const query = `select s.code, s.codeMovie, s.codeRap, s.codeRoom, s.codeShowTime, s.screenAt, s.status, m.movieName as nameMovie, r.name as nameRap,  ro.name as nameRoom, sh.showDate as showDate
+        from showing as s 
+        join movie as m on s.codeMovie = m.code
+        join rap as r on s.codeRap = r.code
+        join room as ro on s.codeRoom = ro.code
+        join showtime as sh on s.codeShowTime = sh.code`;
+  const [allShow, metadata] = await db.sequelize.query(query);
+  return allShow;
+};
+
+const getShowByCodeService = async (code) => {
+  const show = await db.Showing.findOne({
+    where: {
+      code: code,
+    },
+  });
+  return show;
+};
 
 const createShowService = async (show) => {
-    const newShow = await db.Showing.create(show);
-    return newShow;
-}
+  const newShow = await db.Showing.create(show);
+  return newShow;
+};
 
 const checkShow = async (screenAt, codeRap, codeRoom, codeShowTime) => {
-    const query =
-        `select s.code, s.codeMovie, s.codeRap, s.codeRoom, s.codeShowTime, s.screenAt, s.status 
+  const query = `select s.code, s.codeMovie, s.codeRap, s.codeRoom, s.codeShowTime, s.screenAt, s.status 
         from showing as s
-        where s.screenAt = '${screenAt}' and s.codeRap = '${codeRap}'and s.codeRoom = '${codeRoom}' and  s.codeShowTime = '${codeShowTime}';`
-    const [check, metadata] = await db.sequelize.query(query);
-    return check[0];
-}
+        where s.screenAt = '${screenAt}' and s.codeRap = '${codeRap}'and s.codeRoom = '${codeRoom}' and  s.codeShowTime = '${codeShowTime}';`;
+  const [check, metadata] = await db.sequelize.query(query);
+  return check[0];
+};
 
 const getAllShowByMovieService = async (codeMovie) => {
-    const query =
-        `select s.code, s.codeMovie, s.codeRap, s.codeRoom, s.codeShowTime, s.screenAt, s.status, m.movieName, r.name as nameRap,  ro.name as nameRoom, sh.showDate
+  const query = `select s.code, s.codeMovie, s.codeRap, s.codeRoom, s.codeShowTime, s.screenAt, s.status, m.movieName, r.name as nameRap,  ro.name as nameRoom, sh.showDate
         from showing as s 
         join movie as m on s.codeMovie = m.code
         join rap as r on s.codeRap = r.code
         join room as ro on s.codeRoom = ro.code
         join showtime as sh on s.codeShowTime = sh.code
         where s.codeMovie = '${codeMovie}'`;
-    const [allShow, metadata] = await db.sequelize.query(query);
-    return allShow
-}
+  const [allShow, metadata] = await db.sequelize.query(query);
+  return allShow;
+};
 
 const getAllShowByRapService = async (codeRap) => {
-    const query =
-        `select s.code, s.codeMovie, s.codeRap, s.codeRoom, s.codeShowTime, s.screenAt, s.status, m.movieName, r.name as nameRap,  ro.name as nameRoom, sh.showDate
+  const query = `select s.code, s.codeMovie, s.codeRap, s.codeRoom, s.codeShowTime, s.screenAt, s.status, m.movieName, r.name as nameRap,  ro.name as nameRoom, sh.showDate
      from showing as s 
      join movie as m on s.codeMovie = m.code
      join rap as r on s.codeRap = r.code
      join room as ro on s.codeRoom = ro.code
      join showtime as sh on s.codeShowTime = sh.code
-     where s.codeRap = '${codeRap}';`
-    const [allShow, metadata] = await db.sequelize.query(query);
-    return allShow
-}
+     where s.codeRap = '${codeRap}';`;
+  const [allShow, metadata] = await db.sequelize.query(query);
+  return allShow;
+};
 
 module.exports = {
-    getAllShowService,
-    createShowService,
-    getAllShowByMovieService,
-    getAllShowByRapService,
-    checkShow,
-}
+  getAllShowService,
+  getShowByCodeService,
+  createShowService,
+  getAllShowByMovieService,
+  getAllShowByRapService,
+  checkShow,
+};
