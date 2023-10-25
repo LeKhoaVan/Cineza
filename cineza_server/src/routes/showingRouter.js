549674f6b1607e3d185b0d81edb06337d915630f
toduchieu <toduchieu1245@gmail.com>
2023-10-25 16:13:19 +0700

    add update show

--- cineza_server/src/routes/showingRouter.js
+++ cineza_server/src/routes/showingRouter.js
@@ -4,6 +4,7 @@ const {
   getAllShowController,
   getShowByCodeController,
   createShowController,
+  updateShowController,
   getAllShowByMovieController,
   getAllShowByRapController,
 } = require("../controller/showingController");
@@ -13,6 +14,7 @@ const showingRouter = express.Router();
 showingRouter.get("/get-all", getAllShowController);
 showingRouter.get("/get-by-code/:code", getShowByCodeController);
 showingRouter.post("/create", createShowController);
+showingRouter.put("/put/:code", updateShowController);
 showingRouter.get("/get-all-by-movie/:codeMovie", getAllShowByMovieController);
 showingRouter.get("/get-all-by-rap/:codeRap", getAllShowByRapController);
 
