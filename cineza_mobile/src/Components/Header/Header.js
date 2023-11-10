4e05d72126140898e8105f36317dbfb5a701ad1b
LeKhoaVan <lekhoavan325802@gmail.com>
2023-11-10 13:13:06 +0700

    update button

--- cineza_mobile/src/Components/Header/Header.js
+++ cineza_mobile/src/Components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './Style_Header';
-import {View, Text, TouchableOpacity} from 'react-native';
-import {Entypo} from '@react-native-vector-icons';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+// import { Entypo } from 'react-native-vector-icons';
+import iconMenu from "../../assets/imageButton/iconMenu.png";
 // import { FontAwesome5 } from "@expo/vector-icons";
-import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 
 function Header() {
   const navigation = useNavigation();
@@ -20,7 +21,8 @@ function Header() {
       <TouchableOpacity
         onPress={() => navigation.navigate('Menu')}
         style={styles.containerIcon}>
-        <Entypo name="menu" size={25} color="black" />
+        {/* <Entypo name="menu" size={25} color="black" /> */}
+        <Image source={iconMenu} style={{ width: 20, height: 20 }} />
       </TouchableOpacity>
     </View>
   );
